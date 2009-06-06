09020077f9862703f21fdfead9fb764185189b00
Sebastian Schwarz <seschwar@googlemail.com>
2009-06-06 11:30:17 +0200

    Replaced Data.String.Utils.split from MissingH.
    This finally removed all license conflicts and additional dependencies.
    The implementation is a slight generalization of Data.List.words.

--- XLabel.hs
+++ XLabel.hs
@@ -1,8 +1,6 @@
 import Control.Monad (when)
 import Data.Char (isSpace)
-import Data.List (intercalate, isPrefixOf, stripPrefix, union, (\\))
-import Data.Maybe (fromJust)
-import Data.String.Utils (split)
+import Data.List (intercalate, stripPrefix, union, (\\))
 import System.Environment (getArgs)
 
 type Label = String
@@ -11,7 +9,7 @@ main :: IO ()
 main = do
     args <- getArgs
     when (null args) (error "No operation specified.")
-    interact $ unlines . rewrite1 (operator (head args) (tail args)) [] . lines
+    interact $ unlines . rewrite (operator (head args) (tail args)) [] . lines
 
 operator :: Eq a => String -> [a] -> [a] -> [a]
 operator "add"    a b = a `union` b
@@ -20,24 +18,30 @@ operator "remove" a b = b \\ a
 operator "set"    a _ = a
 operator s        _ _ = error $ "Invalid operation: " ++ s
 
-rewrite1 :: ([Label] -> [Label]) -> [Label] -> [String] -> [String]
-rewrite1 f ls ss      | null ss || null (head ss)
-                      = case f . hdr2lst . concat . reverse $ ls of
-                             [] -> ss
-                             l -> ("X-Label: " ++ lst2hdr l) : ss
-rewrite1 f _  (s:ss)  | "X-Label:" `isPrefixOf` s
-                      = rewrite2 f [fromJust $ stripPrefix "X-Label:" s] ss
-rewrite1 f ls (s:ss)  = s : rewrite1 f ls ss
+rewrite :: ([Label] -> [Label]) -> [Label] -> [String] -> [String]
+rewrite f ls ss     | null ss || null (head ss)
+                    = case f . hdr2lst . concat . reverse $ ls of
+                           [] -> ss
+                           l -> ("X-Label: " ++ lst2hdr l) : ss
+rewrite f ls (s:ss) = case stripPrefix "X-Label: " s of
+                           Nothing -> s : rewrite f ls ss
+                           Just s' -> rewrite' f [s'] ss
 
-rewrite2 :: ([Label] -> [Label]) -> [Label] -> [String] -> [String]
-rewrite2 f ls ((c:cs):ss) | isSpace c = rewrite2 f ((c:cs):ls) ss
-rewrite2 f ls ss          = rewrite1 f ls ss
+rewrite' :: ([Label] -> [Label]) -> [Label] -> [String] -> [String]
+rewrite' f ls ((c:cs):ss) | isSpace c = rewrite' f ((c:cs):ls) ss
+rewrite' f ls ss          = rewrite f ls ss
 
 hdr2lst :: String -> [Label]
-hdr2lst = filter (not . null) . map strip . split ","
+hdr2lst = filter (not . null) . map strip . split (== ',')
 
 lst2hdr :: [Label] -> String
 lst2hdr = intercalate ", "
+
+split :: (a -> Bool) -> [a] -> [[a]]
+split p xs = case dropWhile p xs of
+                  []  -> []
+                  xs' -> x : split p xs''
+                      where (x, xs'') = break p xs'
 
 strip :: String -> String
 strip = rstrip . lstrip
