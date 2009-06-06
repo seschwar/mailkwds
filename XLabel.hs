a6dabb67d470f6bc5ef0fe497d1d24ed35fde4bd
Sebastian Schwarz <seschwar@googlemail.com>
2009-06-06 07:08:08 +0200

    Print header even it was not present before.
    The modified header is printed even no if X-Label header was present
    before.

--- XLabel.hs
+++ XLabel.hs
@@ -8,7 +8,7 @@ main :: IO ()
 main = do
     args <- getArgs
     when (null args) (error "No operation specified.")
-    interact $ unlines . rewrite1 (operator (head args) (tail args)) . lines
+    interact $ unlines . rewrite1 (operator (head args) (tail args)) [] . lines
 
 operator :: Eq a => String -> [a] -> [a] -> [a]
 operator "add"    a b = a `union` b
@@ -17,19 +17,17 @@ operator "remove" a b = b \\ a
 operator "set"    a _ = a
 operator s        _ _ = error $ "Invalid operation: " ++ s
 
-rewrite1 :: ([String] -> [String]) -> [String] -> [String]
-rewrite1 _ []      = []
-rewrite1 _ ("":ss) = "":ss
-rewrite1 f (s:ss)  | "X-Label:" `isPrefixOf` s = rewrite2 f ss [s]
-rewrite1 f (s:ss)  = s : rewrite1 f ss
+rewrite1 :: ([String] -> [String]) -> [String] -> [String] -> [String]
+rewrite1 f a ss      | null ss || null (head ss)
+                     = case f . hdr2lst . concat . reverse $ a of
+                            [] -> ss
+                            ls -> ("X-Label: " ++ lst2hdr ls) : ss
+rewrite1 f _ (s:ss)  | "X-Label:" `isPrefixOf` s = rewrite2 f [s] ss
+rewrite1 f a (s:ss)  = s : rewrite1 f a ss
 
 rewrite2 :: ([String] -> [String]) -> [String] -> [String] -> [String]
-rewrite2 f ((c:s):ss) a | isSpace c = rewrite2 f ss ((c:s):a)
-rewrite2 f ss         a = rewrite3 f ss (f . hdr2lst . concat . reverse $ a)
-
-rewrite3 :: ([String] -> [String]) -> [String] -> [String] -> [String]
-rewrite3 f ss [] = rewrite1 f ss
-rewrite3 f ss ls = ("X-Label: " ++ lst2hdr ls) : rewrite1 f ss
+rewrite2 f a ((c:cs):ss) | isSpace c = rewrite2 f ((c:cs):a) ss
+rewrite2 f a ss          = rewrite1 f a ss
 
 hdr2lst :: String -> [String]
 hdr2lst ('X':'-':'L':'a':'b':'e':'l':':':s) = hdr2lst s
