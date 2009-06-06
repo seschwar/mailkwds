4ef689ab9d2fc7d5076a0e58f2037394502674e1
Sebastian Schwarz <seschwar@googlemail.com>
2009-06-06 11:53:23 +0200

    Processes all headers, not just the last one.
    Now all labels from the union of all X-Label headers are processed
    with the input and returned as a single header.

--- XLabel.hs
+++ XLabel.hs
@@ -9,7 +9,7 @@ main :: IO ()
 main = do
     args <- getArgs
     when (null args) (error "No operation specified.")
-    interact $ unlines . rewrite (operator (head args) (tail args)) [] . lines
+    interact $ unlines . rewrite (operator (head args) (tail args)) "" . lines
 
 operator :: Eq a => String -> [a] -> [a] -> [a]
 operator "add"    a b = a `union` b
@@ -18,18 +18,18 @@ operator "remove" a b = b \\ a
 operator "set"    a _ = a
 operator s        _ _ = error $ "Invalid operation: " ++ s
 
-rewrite :: ([Label] -> [Label]) -> [Label] -> [String] -> [String]
-rewrite f ls ss     | null ss || null (head ss)
-                    = case f . hdr2lst . concat . reverse $ ls of
-                           [] -> ss
-                           l -> ("X-Label: " ++ lst2hdr l) : ss
-rewrite f ls (s:ss) = case stripPrefix "X-Label: " s of
-                           Nothing -> s : rewrite f ls ss
-                           Just s' -> rewrite' f [s'] ss
+rewrite :: ([Label] -> [Label]) -> String -> [String] -> [String]
+rewrite f acc ss     | null ss || null (head ss)
+                     = case f $ hdr2lst acc of
+                            [] -> ss
+                            ls -> ("X-Label: " ++ lst2hdr ls) : ss
+rewrite f acc (s:ss) = case stripPrefix "X-Label:" s of
+                            Nothing -> s : rewrite f acc ss
+                            Just s' -> rewrite' f (acc ++ ", " ++ s') ss
 
-rewrite' :: ([Label] -> [Label]) -> [Label] -> [String] -> [String]
-rewrite' f ls ((c:cs):ss) | isSpace c = rewrite' f ((c:cs):ls) ss
-rewrite' f ls ss          = rewrite f ls ss
+rewrite' :: ([Label] -> [Label]) -> String -> [String] -> [String]
+rewrite' f acc (s@(c:_):ss) | isSpace c = rewrite' f (acc ++ s) ss
+rewrite' f acc ss           = rewrite f acc ss
 
 hdr2lst :: String -> [Label]
 hdr2lst = filter (not . null) . map strip . split (== ',')
