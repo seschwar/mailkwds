cf7efe03c5deb39f79bd5602c94f2130ec89e1d9
Sebastian Schwarz <seschwar@googlemail.com>
2009-07-02 23:08:04 +0200

    Split message header and body using break.
    rewrite doesn't have to check for the header-body boundary by itself.

--- XLabel.hs
+++ XLabel.hs
@@ -21,7 +21,8 @@ main :: IO ()
 main = do
     args <- getArgs
     when (null args) (error "No command specified.")
-    interact $ unlines . rewrite (operator (head args) (tail args)) "" . lines
+    interact $ unlines . f (operator (head args) (tail args)) . break (== []) . lines
+        where f op (h, b) = rewrite op "" h ++ b
 
 -- | Chooses the operator to apply to the 'Label's in the mail and the ones
 -- specified as command line arguments.
@@ -33,12 +34,13 @@ operator "set"    a _ = a
 operator "tidy"   _ b = b
 operator s        _ _ = error $ "Invalid command: " ++ s
 
--- | Rewrites the given message by applying the given function to the existing
--- X-Label header fields.  The resulting new header field will be inserted at
--- the end of the header section right before the beginning of the message body.
+-- | Rewrites the given message header by applying the given function to the
+-- existing X-Label header fields.  The resulting new header field will be
+-- inserted at the end.
 rewrite :: ([Label] -> [Label]) -> String -> [String] -> [String]
-rewrite f acc []        = rewrite2 f acc []
-rewrite f acc ss@("":_) = rewrite2 f acc ss
+rewrite f acc []        = case f $ hdr2lst acc of
+                               [] -> []
+                               ls -> ("X-Label: " ++ lst2hdr ls) : []
 rewrite f acc (s:ss)    = case stripPrefix "X-Label:" s of
                                Nothing -> s : rewrite f acc ss
                                Just s' -> rewrite1 f (acc ++ " " ++ s') ss
@@ -48,15 +50,8 @@ rewrite1 :: ([Label] -> [Label]) -> String -> [String] -> [String]
 rewrite1 f acc (s@(c:_):ss) | isSpace c = rewrite1 f (acc ++ s) ss
 rewrite1 f acc ss           = rewrite f acc ss
 
--- | Return X-Label header if it is not empty and the rest of the 'String's.
-rewrite2 :: ([Label] -> [Label]) -> String -> [String] -> [String]
-rewrite2 f acc ss = case f $ hdr2lst acc of
-                         [] -> []
-                         ls -> ("X-Label: " ++ lst2hdr ls) : ss
-
 -- | Parses the 'String' of a comma separated header field body to a list.
 hdr2lst :: String -> [Label]
---hdr2lst = filter (not . null) . map stripWhile isSpace . splitOn (== ',')
 hdr2lst = words
 
 -- | Formats a list of 'Label's so that they can be included as the body of a
