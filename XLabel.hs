377181f029bf1f5e81eee5a167144d099012cc9d
Sebastian Schwarz <seschwar@googlemail.com>
2009-07-25 13:02:44 +0200

    Use Writer Monad for accumulating the labels.
    Also prepared use of multiple keyword/label/tag headers by introducing
    a map from header names to separators.

--- XLabel.hs
+++ XLabel.hs
@@ -7,10 +7,13 @@
 
 module Main where
 
-import Control.Monad (when)
+import Control.Monad (mapM, when)
+import Control.Monad.Writer.Lazy (Writer, runWriter, tell)
 import Data.Char (isSpace)
-import Data.List (intercalate, stripPrefix, union, (\\))
-import Data.List.Split (keepDelimsL, split, whenElt)
+import Data.List (intercalate, union, (\\))
+import Data.List.Split (dropBlanks, dropDelims, keepDelimsL, onSublist, split, whenElt)
+import Data.Map (Map, fromList, lookup)
+import Data.Maybe (Maybe(..), catMaybes)
 import System.Environment (getArgs)
 import Utils (appendWhile)
 
@@ -21,9 +24,10 @@ type Label = String
 -- stdout.
 main :: IO ()
 main = do
+    let m = fromList [("X-Label", " ")]
     args <- getArgs
     when (null args) (error "No command specified.")
-    interact $ unlines . rewriteMsg (operator (head args) (tail args)) . lines
+    interact $ unlines . rewriteMsg m (operator (head args) (tail args)) . lines
 
 -- | Chooses the operator to apply to the 'Label's in the mail and the ones
 -- specified as command line arguments.
@@ -36,25 +40,33 @@ operator "tidy"   _ b = b
 operator s        _ _ = error $ "Invalid command: " ++ s
 
 -- | Rewrites an email message consisting of a tuple heades and body.
-rewriteMsg :: ([Label] -> [Label]) -> [String] -> [String]
-rewriteMsg f msg = let (h, b) = break (== []) msg
-                   in (fldHdr . rewriteHdr f "" . unfldHdr) h ++ b
+rewriteMsg :: Map String String -> ([Label] -> [Label]) -> [String] -> [String]
+rewriteMsg m f msg = let (h, b) = break (== []) msg
+                     in (fldHdr . rewriteHdrs m f . unfldHdr) h ++ b
 
 -- | Appends 'String's beginning with a whitespace character to the
 -- previous 'String' in the list.
 unfldHdr :: [String] -> [String]
 unfldHdr = appendWhile $ const $ isSpace . head
 
--- | Rewrites the given message header by applying the given function to the
--- existing X-Label header fields.  The resulting new header field will be
--- inserted at the end.
-rewriteHdr :: ([Label] -> [Label]) -> String -> [String] -> [String]
-rewriteHdr f acc []     = case f $ toLabels " " acc of
-                               [] -> []
-                               ls -> ["X-Label: " ++ toHeader " " ls]
-rewriteHdr f acc (s:ss) = case stripPrefix "X-Label:" s of
-                               Nothing -> s : rewriteHdr f acc ss
-                               Just s' -> rewriteHdr f (acc ++ " " ++ s') ss
+-- | Rewrite the headers of a message by appending the extraced 'Label's if they
+-- are not empty.
+rewriteHdrs :: Map String String -> ([Label] -> [Label]) -> [String] -> [String]
+rewriteHdrs m f hs = let (hs', ls) = runWriter $ mapM (extractLabels m) hs
+                     in case f ls of
+                             []  -> catMaybes hs'
+                             ls' -> catMaybes hs'
+                                    ++ ["X-Label: " ++ toHeader " " ls']
+
+-- | Extracts the 'Label's of a single header by 'tell'ing them to a 'Writer'
+-- 'Monad' and dropping them from the message by replacing them with 'Nothing'.
+extractLabels :: Map String String -> String -> Writer [Label] (Maybe String)
+extractLabels m h = case break (== ':') h of
+                         (n, ':':b) -> case Data.Map.lookup n m of
+                                            Nothing  -> return $ Just h
+                                            Just sep -> tell (toLabels sep b)
+                                                        >> return Nothing
+                         _          -> return $ Just h
 
 -- | Parses the 'String' of a comma separated header field body to a list.
 toLabels :: String -> String -> [Label]
