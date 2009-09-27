a2c3c82f0025c596cdab624e86afd4a3fc866fb0
Sebastian Schwarz <seschwar@googlemail.com>
2009-09-27 17:31:55 +0200

    Added a few properties for testing header folding.

--- test/Test.hs
+++ test/Test.hs
@@ -7,13 +7,33 @@
 
 module Main where
 
+import Data.List (all)
 import Test.Framework (Test, defaultMain, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck
+import Utils
+import XLabel
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: [Test]
-tests = []
+tests =
+    [ testGroup "header folding"
+        [ testProperty "fold1" prop_fold1
+        , testProperty "fold2" prop_fold2
+        , testProperty "fold3" prop_fold3
+        , testProperty "fold4" prop_fold4
+        ]
+    ]
+
+prop_fold1 x = all (not . null) x ==>
+    (unfoldHeaders . unfoldHeaders $ x) == unfoldHeaders x
+prop_fold2 x = all (not . null) x ==>
+    (foldHeaders . foldHeaders $ x) == foldHeaders x
+prop_fold3 x = all (not . null) x ==>
+    (unfoldHeaders . foldHeaders . unfoldHeaders $ x) == unfoldHeaders x
+prop_fold4 x = all (not . null) x ==>
+    (unfoldHeaders . foldHeaders . unfoldHeaders . foldHeaders $ x)
+    == (unfoldHeaders . foldHeaders $ x)
 
