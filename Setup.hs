1e909a4b9038d70ad92e6b6ddd0b32c28c2d8e63
Sebastian Schwarz <seschwar@googlemail.com>
2009-10-13 15:36:24 +0200

    Added pandoc preprocessor.
    However, Cabal invokes them only for missing source files during the
    compilation, not for missing data files.

--- Setup.hs
+++ Setup.hs
@@ -2,11 +2,24 @@
 
 import Distribution.Simple
 import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.PreProcess
 import System.FilePath ((</>))
 import System.Cmd (rawSystem)
 
-main = defaultMainWithHooks simpleUserHooks { runTests = runTests' }
+main = defaultMainWithHooks simpleUserHooks
+    { runTests            = runTests'
+    , hookedPreProcessors = [("md", ppPandoc), ("text", ppPandoc)]
+    }
 
 runTests' _ _ _ lbi = rawSystem ((buildDir lbi) </> "test" </> "test")
                       ["--maximum-generated-tests=1000"] >> return ()
+
+ppPandoc _ _ = PreProcessor
+    { platformIndependent = True
+    , runPreProcessor     = mkSimplePreProcessor $ \input output verbosity -> do
+--      info verbosity (input ++ " preprocessed to " ++ output)
+        rawSystem "pandoc" ["-sS" ,"--from=markdown", "--to=man", "--output",
+                            output, input]
+        return ()
+    }
 
