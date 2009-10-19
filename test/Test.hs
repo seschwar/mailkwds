7abdc18e80dce5884e4e0c7c68b2e142853a5f10
Sebastian Schwarz <seschwar@googlemail.com>
2009-10-19 20:21:01 +0200

    Added a few more QuickCheck properties.
    Unnecessary but interesting usage of liftM2.  See:
    http://www.haskell.org/pipermail/beginners/2009-October/002448.html

--- test/Test.hs
+++ test/Test.hs
@@ -7,6 +7,8 @@
 
 module Main where
 
+import Control.Monad (liftM2)
+import Data.Char (isSpace)
 import Data.List (all)
 import Test.Framework (Test, defaultMain, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
@@ -19,21 +21,35 @@ main = defaultMain tests
 
 tests :: [Test]
 tests =
-    [ testGroup "header folding"
+    [ testGroup "utils"
+        [ testProperty "cat1" prop_cat1
+        , testProperty "cat2" prop_cat1
+        ]
+    , testGroup "header folding"
         [ testProperty "fold1" prop_fold1
         , testProperty "fold2" prop_fold2
         , testProperty "fold3" prop_fold3
         , testProperty "fold4" prop_fold4
+        , testProperty "fold5" prop_fold5
+        , testProperty "fold6" prop_fold6
         ]
     ]
 
-prop_fold1 x = all (not . null) x ==>
-    (unfoldHeaders . unfoldHeaders $ x) == unfoldHeaders x
-prop_fold2 x = all (not . null) x ==>
-    (foldHeaders . foldHeaders $ x) == foldHeaders x
-prop_fold3 x = all (not . null) x ==>
-    (unfoldHeaders . foldHeaders . unfoldHeaders $ x) == unfoldHeaders x
-prop_fold4 x = all (not . null) x ==>
-    (unfoldHeaders . foldHeaders . unfoldHeaders . foldHeaders $ x)
-    == (unfoldHeaders . foldHeaders $ x)
+prop_cat1 xs = (not . null) (xs :: [[Int]]) ==>
+    concatWhile (const $ const True) xs == [concat xs]
+prop_cat2 xs = concatWhile (const $ const False) xs == xs
+
+prop_fold1 xs = all (not . null) xs ==>
+    (unfoldHeaders . unfoldHeaders $ xs) == unfoldHeaders xs
+prop_fold2 xs = all (not . null) xs ==>
+    (foldHeaders . foldHeaders $ xs) == foldHeaders xs
+prop_fold3 xs = all (not . null) xs ==>
+    (unfoldHeaders . foldHeaders . unfoldHeaders $ xs) == unfoldHeaders xs
+prop_fold4 xs = all (not . null) xs ==>
+    (unfoldHeaders . foldHeaders . unfoldHeaders . foldHeaders $ xs)
+    == (unfoldHeaders . foldHeaders $ xs)
+prop_fold5 xs = all f $ foldHeaders xs
+    where f = liftM2 (||) ((<= 78) . length) (not . any isSpace . lstrip)
+prop_fold6 xs = all (not . null) xs ==>
+    all (not . isSpace) . map head $ unfoldHeaders xs
 
