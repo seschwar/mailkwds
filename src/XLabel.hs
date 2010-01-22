1872309626cc0d0613ee60dcfc14e6dc9c896a6f
Sebastian Schwarz <seschwar@googlemail.com>
2010-01-22 22:54:45 +0100

    Added --help and --version options and their messages.

--- src/XLabel.hs
+++ src/XLabel.hs
@@ -9,8 +9,10 @@
 
 module Main where
 
+import Control.Monad (when)
 import Data.List (nub)
 import System.Environment (getArgs)
+import System.Exit (exitSuccess)
 import XLabel.Core (rewriteMsg, toHeaders)
 import XLabel.Args (Config(..), parseArgs)
 
@@ -20,8 +22,39 @@ main :: IO ()
 main = do
     args <- getArgs
     let !config = case parseArgs args of
-                       Left  msg -> error msg
+                       Left  msg -> error $ msg ++ "\n\n" ++ helpMessage
                        Right cfg -> cfg
+    when (help config) (putStr helpMessage >> exitSuccess)
+    when (version config) (putStr versionMessage >> exitSuccess)
     let f = toHeaders (output config) . (command config $ labels config) . nub
     interact $ unlines . rewriteMsg (input config) f . lines
+
+helpMessage:: String
+helpMessage = unlines $
+              [ "Usage: x-label [OPTION]... [--] COMMAND [LABEL]..."
+              , "Change the set of labels in the specified headers."
+              , ""
+              , "Options:"
+              , "    -c, --catenate  catenate folded headers"
+              , "    -h, --help      print usage information"
+              , "    -i HEADER SEP, --input HEADER SEP"
+              , "                    read labels listed in HEADER separated by SEP"
+              , "    -o HEADER SEP, --output HEADER SEP"
+              , "                    print HEADER with a list of labels separated by SEP"
+              , "    -v, --version   print version information"
+              , "Commands:"
+              , "    add             add LABELs"
+              , "    clear           remove all labels"
+              , "    remove          remove LABELs"
+              , "    set             replace with LABELs"
+              , "    tidy            remove duplicated labels"
+              ]
+
+versionMessage :: String
+versionMessage = unlines $
+                 [ "x-label 0.0"
+                 , "Copyright (C) 2009-2010, Sebastian Schwarz"
+                 , "This is free software licensed under the ISC license."
+                 , "There is absolutely no warranty."
+                 ]
 
--- src/XLabel/Args.hs
+++ src/XLabel/Args.hs
@@ -20,10 +20,12 @@ import XLabel.Utils (token)
 -- | The configuration options for the program.
 data Config a = Config
     { catenate :: Bool                -- ^ whether to catenate or refold headers
-    , input    :: Map String String   -- ^ the headers to parse
-    , output   :: [(String, String)]  -- ^ the headers to print
     , command  :: [a] -> [a] -> [a]   -- ^ the command to apply to the 'Label's
+    , help     :: Bool
+    , input    :: Map String String   -- ^ the headers to parse
     , labels   :: [a]                 -- ^ the 'Label's to be added/removed
+    , output   :: [(String, String)]  -- ^ the headers to print
+    , version  :: Bool
     }
 
 instance Show a => Show (Config a) where
@@ -37,15 +39,17 @@ instance Show a => Show (Config a) where
 
 -- | Parse the command line arguments and create an appropriate configuration.
 parseArgs :: [String] -> Either String (Config Label)
-parseArgs args = ((++) "Unable to parse command line arguments: " . show)
+parseArgs args = ((++) "Unable to parse command line arguments:\n" . show)
                  +++ (sanitizeConfig . \x -> foldr (>>>) id x $ config)
                  $ parse pArgs args  -- (>>>) ensures that the map's values get
     where config = Config            -- overwritten correctly
               { catenate = False
-              , input    = empty
-              , output   = []
               , command  = flip const
+              , help     = False
+              , input    = empty
               , labels   = []
+              , output   = []
+              , version  = False
               }
 
 -- | Ensure a halfway sane configuration.
@@ -65,7 +69,29 @@ pArgs = many pOption <+> pSep <+> fmap (:[]) pCommand <+> many pLabel
     where (<+>) = liftA2 (++)
 
 pOption :: Parser [String] (Config Label -> Config Label)
-pOption = pCat <|> pInput <|> pOutput <?> "option"
+pOption = pCat <|> pHelp <|> pInput <|> pOutput <|> pVersion <?> "option"
+
+pCat :: Parser [String] (Config a -> Config a)
+pCat = (token "-c" <|> token "--cat") *> pure (cat True) <?> "catenate"
+    where cat x c = c { catenate = x }
+
+pHelp :: Parser [String] (Config a -> Config a)
+pHelp = (token "-h" <|> token "--help") *> pure (hel True) <?> "help"
+    where hel x c = c { help = x }
+
+pInput :: Parser [String] (Config Label -> Config Label)
+pInput = (token "-i" <|> token "--input") *> (inp <$> anyToken <*> anyToken)
+         <?> "input"
+    where inp x y c = c { input = insert (map toLower x) y $ input c }
+
+pOutput :: Parser [String] (Config Label -> Config Label)
+pOutput = (token "-o" <|> token "--output") *> (out <$> anyToken <*> anyToken)
+          <?> "output"
+    where out x y c = c { output = (x, y) : output c }
+
+pVersion :: Parser [String] (Config a -> Config a)
+pVersion = (token "-v" <|> token "--version") *> pure (ver True) <?> "version"
+    where ver x c = c { version = x }
 
 pSep :: Parser [String] [Config a -> Config a]
 pSep = skip (token "--") *> pure [] <?> "separator"
@@ -85,18 +111,4 @@ pCommand = (<?> "command") . choice . map (\(c, f) -> token c *> pure (com f))
 pLabel :: Parser [String] (Config Label -> Config Label)
 pLabel = lab <$> anyToken <?> "label"
     where lab x c = c { labels = x : labels c }
-
-pCat :: Parser [String] (Config a -> Config a)
-pCat = (token "-c" <|> token "--cat") *> pure (cat True) <?> "catenate"
-    where cat x c = c { catenate = x }
-
-pInput :: Parser [String] (Config Label -> Config Label)
-pInput = (token "-i" <|> token "--input") *> (inp <$> anyToken <*> anyToken)
-         <?> "input"
-    where inp x y c = c { input = insert (map toLower x) y $ input c }
-
-pOutput :: Parser [String] (Config Label -> Config Label)
-pOutput = (token "-o" <|> token "--output") *> (out <$> anyToken <*> anyToken)
-          <?> "output"
-    where out x y c = c { output = (x, y) : output c }
 
